ea9ac2432f09993cb74a7283887c30bd22fa1d59
junk-d0g <liammac0523@gmail.com>
2024-02-15 12:12:18 -0500

    Added comments

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 This is just an easy wallpaper daily task I made
-
+I need to add a cleaner for the leftover pics
--- wallpaper.py
+++ wallpaper.py
@@ -8,9 +8,13 @@ page = requests.get(url).text
 soup = BeautifulSoup(page, "html.parser")
 images = soup.findAll('img', alt=True)
 
+#couldnt find a better way to get specifically the newest photo of the day
+#first two images are logos, third is newest
 imgUrl = str(images[2])
 newUrl = imgUrl[imgUrl.find("https"):imgUrl.find("D/")+2]
 photoNum = imgUrl[imgUrl.find("D/")+2:imgUrl.find(".T")]
+#for some reason, bs.find is finding the thumbnail version
+#but the real photo can be found with some string manip
 photoNum = str(int(photoNum)-1)
 photoNum += ".jpg"
 newUrl += photoNum
